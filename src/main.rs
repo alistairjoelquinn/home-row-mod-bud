4c4923d49da1315920a0f2a9fcdc0fa5bbdb5db9
alistairjoelquinn <alistairjoelquinn@gmail.com>
2026-01-25 22:06:53 +0100

    adds basic app structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{Text, text};
+
+#[derive(Default)]
+struct App {}
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+impl App {
+    fn update(&mut self, message: Message) {
+        println!("{:#?}", message)
+    }
+    fn view(&self) -> Text<'_> {
+        text("hello!").size(50)
+    }
+}
+
+fn main() -> iced::Result {
+    iced::run(App::update, App::view)
 }
